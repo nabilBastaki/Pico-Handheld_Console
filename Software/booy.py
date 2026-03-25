ad0621c0aa1bbf79aa1be55e77bc4aaa8f10381c
Nabil Albastaki <nabil_bastaki@yahoo.com>
2026-03-25 15:14:37 +0400

    Delete Software/booy.py

--- Software/booy.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin, Timer
-import utime
-led = Pin("LED", Pin.OUT)
-timer = Timer()
-
-def ledblink(timer):
-    led.toggle()
-    
-timer.init(freq=1, mode=Timer.PERIODIC, callback=ledblink)
